db9e44bd1e9ae43fc4d9279385bfb4b3476c8e30
joshdgeek <mhiztaaustin@gmail.com>
2025-06-14 18:56:42 +0100

    dotenv

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express();
 const route = require("./routes/routes.js")
-const cors = require("cors")
+const cors = require("cors");
+const dotenv = require(dotenv);
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 
 app.use(route);
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`listening at port: ${port}`)
